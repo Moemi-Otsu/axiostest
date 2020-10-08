730bf1286c7fb22f21634e94104176ac842c03d0
moemi ohtsu <s0623192@gmail.com>
2020-10-08 23:54:15 +0900

    wip: 238. Interceptorsを使って、thenやcatchの前に共通で行いたい処理を書く

--- src/main.js
+++ src/main.js
@@ -10,6 +10,28 @@ axios.defaults.baseURL =
 // axios.defaults.headers.common["Authorization"] = "asdfasdfasdf";
 // axios.defaults.headers.get["Accept"] = "application/json";
 
+// リクエストと処理が帰ってくる間に共通の処理を入れることができるinterceptors
+// サーバーにリクエストを送る前に処理入れるrequest
+axios.interceptors.request.use(
+  config => {
+    return config;
+  },
+  error => {
+    // これだとthenに入ってしまう。
+    //  return error;
+    // 下記のようにかくと、catchに入る。
+    return Promise.reject(error);
+  }
+);
+// サーバーからデータをとってきてthenに渡す前に処理入れるresponse
+axios.interceptors.response.use(
+  config => {
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  });
+
 new Vue({
   render: h => h(App),
 }).$mount('#app');
